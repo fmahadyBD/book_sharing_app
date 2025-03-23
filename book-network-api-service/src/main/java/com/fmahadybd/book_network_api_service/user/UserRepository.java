689d9fdc3fb73cec7de97b4a97842f7592c95bba
fmahadyBD <fmahady01@gmail.com>
2025-03-23 18:43:57 +0600

    complete Security,register,email,docker and others

---
+++ book-network-api-service/src/main/java/com/fmahadybd/book_network_api_service/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fmahadybd.book_network_api_service.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String username);
+}
