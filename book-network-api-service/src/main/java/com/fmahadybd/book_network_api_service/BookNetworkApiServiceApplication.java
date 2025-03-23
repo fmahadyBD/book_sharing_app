ccc08859fe3226441c9ff8fd32b02d31d9824e58
fmahadyBD <fmahady01@gmail.com>
2025-03-23 16:04:08 +0600

    Initial Commit

---
+++ book-network-api-service/src/main/java/com/fmahadybd/book_network_api_service/BookNetworkApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.book_network_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiServiceApplication.class, args);
+	}
+
+}
---
+++ book-network-api-service/src/test/java/com/fmahadybd/book_network_api_service/BookNetworkApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.book_network_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
