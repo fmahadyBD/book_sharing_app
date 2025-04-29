d2d902aad440b23e6013b84bfca52dac73ba8354
fmahadyBD <fmahady01@gmail.com>
2025-04-29 19:53:43 +0600

    Problem in the angular project so be sure next time

--- book-service-uif/src/app/app.component.spec.ts
+++ book-service-uif/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'book-network-ui' title`, () => {
+  it(`should have as title 'book-service-uif'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-network-ui');
+    expect(app.title).toEqual('book-service-uif');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-network-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-service-uif');
   });
 });
