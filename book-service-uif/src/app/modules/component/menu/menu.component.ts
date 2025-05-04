2299d3403eaff950ab1a2e2b633ba9ee7570ab3b
fmahadyBD <fmahady01@gmail.com>
2025-05-04 09:27:07 +0600

    complete the activate link with js

--- book-service-uif/src/app/modules/component/menu/menu.component.ts
+++ book-service-uif/src/app/modules/component/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach(link => {
+      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkColor.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+      });
+    });
+
+  }
 
 
-  logout(){
-    
+  logout() {
+
   }
 }
