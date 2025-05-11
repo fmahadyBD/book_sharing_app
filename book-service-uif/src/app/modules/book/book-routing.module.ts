d6d2be097e257082e64f6705ea7247c5070479b0
fmahadyBD <fmahady01@gmail.com>
2025-05-11 21:49:11 +0600

    complete the returned my books

--- book-service-uif/src/app/modules/book/book-routing.module.ts
+++ book-service-uif/src/app/modules/book/book-routing.module.ts
@@ -5,6 +5,7 @@ import { BookListComponent } from './pages/book-list/book-list.component';
 import { MyBooksComponent } from './my-books/my-books.component';
 import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
+import { ReturnedBookComponent } from './pages/returned-book/returned-book.component';
 
 const routes: Routes = [
   {path:'',
@@ -29,6 +30,9 @@ const routes: Routes = [
         path: 'my-borrowed-books',
         component: BorrowedBookListComponent
       
+      },{
+        path:'my-returned-books',
+        component: ReturnedBookComponent
       }
 
     ]
